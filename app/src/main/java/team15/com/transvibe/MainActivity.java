fa136070971c2b7683701d19a31d7ce149723135
maliha93 <maliha.islam@yahoo.com>
2016-05-09 22:28:29 +0600

    added new stuff

--- app/src/main/java/team15/com/transvibe/MainActivity.java
+++ app/src/main/java/team15/com/transvibe/MainActivity.java
@@ -1,9 +1,11 @@
 package team15.com.transvibe;
 
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,10 +13,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button n[]=new Button [4];
+        n[0]=(Button) findViewById(R.id.button1);
+        n[1]=(Button) findViewById(R.id.button2);
+        n[2]=(Button) findViewById(R.id.button3);
+        n[3]=(Button) findViewById(R.id.button4);
+
+        Typeface typeface = Typeface.createFromAsset(getAssets(), "Xenotron.ttf");
+        for(int i=0;i<4;i++)
+            n[i].setTypeface(typeface);
+
     }
 
     public void clickedStarter(View view) {
         Intent intent = new Intent(MainActivity.this, WaveActivity.class);
+        startActivity(intent);
+    }
+    public void onClickInput(View view){
+        Intent intent = new Intent(this, input.class);
         startActivity(intent);
     }
 }
