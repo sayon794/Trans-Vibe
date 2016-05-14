33afa81fc3129644bef7e78481c12eaa07196721
tausiftt5238 <tausiftt5238@gmail.com>
2016-05-14 20:00:49 +0600

    Added maliha's changes as well

--- app/src/main/java/team15/com/transvibe/credits.java
+++ app/src/main/java/team15/com/transvibe/credits.java
@@ -3,20 +3,11 @@ package team15.com.transvibe;
 /**
  * Created by maliha on 5/12/16.
  */
-import android.content.Intent;
 import android.graphics.Typeface;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
-import android.widget.Button;
 import android.widget.TextView;
-import android.widget.EditText;
-import android.text.TextUtils;
-import android.content.Context;
-import android.widget.Spinner;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 
@@ -25,11 +16,16 @@ public class credits extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_credits);
 
-        /*TextView tv= (TextView)findViewById(R.id.textView);
+        TextView tv[]=new TextView[5];
+        tv[0]= (TextView)findViewById(R.id.sayon);
+        tv[1]= (TextView)findViewById(R.id.maliha);
+        tv[2]= (TextView)findViewById(R.id.tahsin);
+        tv[3]= (TextView)findViewById(R.id.pritom);
+        tv[4]= (TextView)findViewById(R.id.team15);
 
-        Animation translatebu= AnimationUtils.loadAnimation(this, R.anim.animationfile);
-        tv.setText("Some text view.");
-        tv.startAnimation(translatebu);*/
+        Animation translatebu= AnimationUtils.loadAnimation(this, R.anim.bounce);
+        for(int i=0;i<4;i++)
+            tv[i].startAnimation(translatebu);
 
 
 
